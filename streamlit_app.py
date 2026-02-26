83def25b7e39f92d37c5c31a05ed70776cea7253
Laksh Amarnani <Lakshamarnani19@gmail.com>
2026-02-26 18:33:46 +0530

    Created the main streamlit_app.py and imported all the required packages.

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime, timedelta
+import yfinance as yf
+from scipy import stats
+
